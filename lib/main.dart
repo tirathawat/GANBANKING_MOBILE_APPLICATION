80416acbf40817df17ddbb5f3dd29dff15c99d98
tirathawat@gmail.com <tirathawat@gmail.com>
2021-06-01 00:33:39 +0700

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:ganbanking/config/app.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp().then((value) => print("INITIALIZED FIREBASE"));
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.transparent,
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark));
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(App());
 }
